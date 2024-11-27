53b5ae1906fc5a3518558dbc8d2b37a5f01e8aa0
technicalclipper <technicalclipper@gmail.com>
2024-11-28 00:40:31 +0530

    login functionality

--- frontend/app/src/pages/login.jsx
+++ frontend/app/src/pages/login.jsx
@@ -1,11 +1,23 @@
-function login(){
+import { useState } from "react";
+function Login(){
     const [userDetails,setuserDetails]=useState({username:"",password:""});
+
+    function updateinfo(e){
+        const name=e.target.name;
+        const value=e.target.value;
+        setuserDetails((prev)=>{
+            return({...prev,[name]:value});
+        })
+    }
 
     return(
         <div className="login-container">
             <div>Username</div>
-            <input>
+            <input name="username" value={userDetails.username} placeholder="enter your username" onChange={updateinfo}/>
             <div>Password</div>
+            <input name="password" value={userDetails.password} placeholder="enter your password" onChange={updateinfo}/>
         </div>
     )
 }
+
+export default Login;
