c8315f0777f3c3ca0a5d78bf91c194c7ff575b1c
technicalclipper <technicalclipper@gmail.com>
2024-11-28 19:15:07 +0530

    renamed

--- frontend/app/src/components/header.jsx
+++ frontend/app/src/components/header.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom"
 export default function Header(){
+    const isAuthenticated=false ;
     return(
         <div className="header">
         <div className="navtitle">ecommerce</div>
         <div className="login-cont">
-            <Link to="/login"><button className="header-login">login</button></Link>
-            <Link to="/signup"><button className="header-login"> signup</button></Link>
+            {!isAuthenticated?
+            (<><Link to="/login"><button className="header-login">login</button></Link>
+            <Link to="/signup"><button className="header-login"> signup</button></Link></>):
+            (<><Link to="/signup"><button className="header-login">product</button></Link>
+            <Link to="/login"><button className="header-login">logout</button></Link>
+            <Link to="/signup"><button className="header-login"> cart</button></Link></>)}
         </div>
         </div>
     )
